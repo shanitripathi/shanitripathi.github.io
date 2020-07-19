7d9a92849e40f95cd2d97ff7dbc8428f2d32b497
shani__tripathi <shani.tripathi01@gmail.com>
2020-07-19 21:43:00 +0100

    css added

--- tic-tac-toe-react/src/App.js
+++ tic-tac-toe-react/src/App.js
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home";
 
-function App() {
-  return (
-    <div className="App">
-      <Home />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    marks: ["x", "o", "x", "o", "x", "o", "x", "o", "x"],
+  };
+  render() {
+    return (
+      <div className="App">
+        <Home marks={this.state.marks} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- tic-tac-toe-react/src/components/Home.js
+++ tic-tac-toe-react/src/components/Home.js
@@ -1,49 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Home = () => {
-  return (
-    <div class="container mb-2">
-      <div class="line-1 line">
-        <div class="left">
-          <button class="cell" id="1"></button>
+class Home extends React.Component {
+  handleClick = (e) => {
+    e.target.innerHTML = this.props.marks[0];
+  };
+  render() {
+    return (
+      <div className="container mb-2">
+        <div className="line-1 line">
+          <div className="left">
+            <button className="cell" id="1" onClick={this.handleClick}></button>
+          </div>
+          <div className="middle">
+            <button className="cell" id="2"></button>
+          </div>
+          <div className="right">
+            <button className="cell" id="3"></button>
+          </div>
         </div>
-        <div class="middle">
-          <button class="cell" id="2"></button>
+        <div className="line-2 line">
+          <div className="left">
+            <button className="cell" id="4"></button>
+          </div>
+          <div className="middle">
+            <button className="cell" id="5"></button>
+          </div>
+          <div className="right">
+            <button className="cell" id="6"></button>
+          </div>
         </div>
-        <div class="right">
-          <button class="cell" id="3"></button>
+        <div className="line-3 line">
+          <div className="left">
+            <button className="cell" id="7"></button>
+          </div>
+          <div className="middle">
+            <button className="cell" id="8"></button>
+          </div>
+          <div className="right">
+            <button className="cell" id="9"></button>
+          </div>
         </div>
+        <div>
+          <button id="undobutton">
+            <h6 className="p-0 m-0">UNDO</h6>
+          </button>
+        </div>
+        <h1 className="demo"></h1>
       </div>
-      <div class="line-2 line">
-        <div class="left">
-          <button class="cell" id="4"></button>
-        </div>
-        <div class="middle">
-          <button class="cell" id="5"></button>
-        </div>
-        <div class="right">
-          <button class="cell" id="6"></button>
-        </div>
-      </div>
-      <div class="line-3 line">
-        <div class="left">
-          <button class="cell" id="7"></button>
-        </div>
-        <div class="middle">
-          <button class="cell" id="8"></button>
-        </div>
-        <div class="right">
-          <button class="cell" id="9"></button>
-        </div>
-      </div>
-      <div>
-        <button id="undobutton">
-          <h6 class="p-0 m-0">UNDO</h6>
-        </button>
-      </div>
-      <h1 class="demo"></h1>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default Home;
