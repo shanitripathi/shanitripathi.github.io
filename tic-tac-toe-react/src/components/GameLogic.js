838ce45d0fabdfbdc3d0ba0b387c0ee4cc11a449
shani__tripathi <shani.tripathi01@gmail.com>
2020-07-30 11:40:24 +0100

    Board designed

--- tic-tac-toe-react/src/components/GameLogic.js
+++ tic-tac-toe-react/src/components/GameLogic.js
@@ -8,15 +8,35 @@ class GameLogic extends Component {
     currentPlayer: "o",
   };
 
-  handleClick = () => {
+  winCheck = () => {
     let { boardState, currentPlayer } = this.state;
-    if (boardState[2] === "") {
-      boardState[2] = currentPlayer;
+    if (
+      ((boardState[0] === boardState[1]) === boardState[2]) == "o" ||
+      (boardState[0] === boardState[3]) === boardState[6] ||
+      (boardState[0] === boardState[4]) === boardState[8] ||
+      (boardState[1] === boardState[4]) === boardState[7] ||
+      (boardState[3] === boardState[4]) === boardState[6] ||
+      (boardState[3] === boardState[5]) === boardState[8] ||
+      (boardState[3] === boardState[4]) === boardState[5] ||
+      (boardState[6] === boardState[7]) === boardState[8]
+    ) {
+      console.log("Player Wins");
+    }
+    // else if (!("" in boardState)) {
+    //   console.log("It is a draw");
+    // }
+  };
+
+  handleClick = (index) => {
+    let { boardState, currentPlayer } = this.state;
+    if (boardState[index] === "") {
+      boardState[index] = currentPlayer;
       this.setState({
         currentPlayer: currentPlayer === "o" ? "x" : "o",
       });
     }
     console.log(boardState);
+    this.winCheck();
   };
   render() {
     return (
--- tic-tac-toe-react/src/screens/Game.js
+++ tic-tac-toe-react/src/screens/Game.js
@@ -3,6 +3,16 @@ import rectangle from "../images/rectangle.svg";
 import oval from "../images/oval.svg";
 
 class Game extends Component {
+  boardMap = (id) => {
+    let { boardState, handleClick } = this.props;
+    if (boardState[id] === "") {
+      return <h1></h1>;
+    } else if (boardState[id] === "o") {
+      return <h1>O</h1>;
+    } else if (boardState[id] === "x") {
+      return <h1>X</h1>;
+    }
+  };
   render() {
     let { boardState, handleClick } = this.props;
     return (
@@ -23,20 +33,88 @@ class Game extends Component {
               <button
                 className="btn-game"
                 id="1"
-                onClick={handleClick}
-              ></button>
-              <button className="btn-game" id="2"></button>
-              <button className="btn-game" id="3"></button>
+                onClick={() => {
+                  handleClick(0);
+                }}
+              >
+                {this.boardMap(0)}
+              </button>
+              <button
+                className="btn-game"
+                id="2"
+                onClick={() => {
+                  handleClick(1);
+                }}
+              >
+                {this.boardMap(1)}
+              </button>
+              <button
+                className="btn-game"
+                id="3"
+                onClick={() => {
+                  handleClick(2);
+                }}
+              >
+                {this.boardMap(2)}
+              </button>
             </div>
             <div className="game-line">
-              <button className="btn-game" id="4"></button>
-              <button className="btn-game" id="5"></button>
-              <button className="btn-game" id="6"></button>
+              <button
+                className="btn-game"
+                id="4"
+                onClick={() => {
+                  handleClick(3);
+                }}
+              >
+                {this.boardMap(3)}
+              </button>
+              <button
+                className="btn-game"
+                id="5"
+                onClick={() => {
+                  handleClick(4);
+                }}
+              >
+                {this.boardMap(4)}
+              </button>
+              <button
+                className="btn-game"
+                id="6"
+                onClick={() => {
+                  handleClick(5);
+                }}
+              >
+                {this.boardMap(5)}
+              </button>
             </div>
             <div className="game-line">
-              <button className="btn-game" id="7"></button>
-              <button className="btn-game" id="8"></button>
-              <button className="btn-game" id="9"></button>
+              <button
+                className="btn-game"
+                id="7"
+                onClick={() => {
+                  handleClick(6);
+                }}
+              >
+                {this.boardMap(6)}
+              </button>
+              <button
+                className="btn-game"
+                id="8"
+                onClick={() => {
+                  handleClick(7);
+                }}
+              >
+                {this.boardMap(7)}
+              </button>
+              <button
+                className="btn-game"
+                id="9"
+                onClick={() => {
+                  handleClick(8);
+                }}
+              >
+                {this.boardMap(8)}
+              </button>
             </div>
           </div>
           <div class="col-12 col-lg-3 text-center text-lg-right">
