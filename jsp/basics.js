8a0bb787af762c0b8c38aa2b5f9be76040427690
shani__tripathi <shani.tripathi01@gmail.com>
2020-09-03 19:57:38 +0100

    design  changes

---
+++ jsp/basics.js
@@ -0,0 +1,15 @@
+a = [1, 2, 3, 4, 4];
+
+// for (i = 0; i < a.length; i++) {
+//   console.log(a[i]);
+// }
+
+// for (let name of a) {
+//   console.log(name);
+// }
+
+// a.forEach(function (number) {
+//   console.log("welcome number " + number);
+// });
+
+console.log(window);
