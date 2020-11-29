b8176863f1b690cfcb38a962ee1b7ef6abe62740
shani__tripathi <shani.tripathi01@gmail.com>
2020-11-29 19:31:41 +0000

    design changes

--- main.js
+++ main.js
@@ -1,46 +1,56 @@
-const headerName = document.querySelector(".header-name");
+if (window.location.pathname === "/index.html") {
+  const headerName = document.querySelector(".header-name");
 
-const text = "Hi, I am Shani!";
-const speed = 50;
-var i = 0;
+  const text = "Hi, I am Shani!";
+  const speed = 50;
+  var i = 0;
 
-function animateHeader() {
-  if (i < text.length) {
-    headerName.innerHTML += text[i];
-    i++;
+  function animateHeader() {
+    if (i < text.length) {
+      headerName.innerHTML += text[i];
+      i++;
 
-    setTimeout(animateHeader, speed);
+      setTimeout(animateHeader, speed);
+    }
   }
+
+  animateHeader();
+
+  const columns = document.querySelectorAll(".columns");
+  const controller = new ScrollMagic.Controller();
+  const absorb = gsap.timeline({
+    defaults: {
+      duration: 0.5,
+    },
+  });
+  columns.forEach((column) => {
+    absorb.fromTo(
+      column,
+      { opacity: 0, y: "10%", scale: 0.3 },
+      { opacity: 1, y: "0", scale: 1 }
+    );
+
+    absorbScene = new ScrollMagic.Scene({
+      triggerElement: column,
+      triggerHook: 1,
+      reverse: false,
+    })
+
+      .setTween(absorb)
+      .addTo(controller);
+  });
+
+  const navButton = document.querySelector(".navbar-toggler");
+
+  navButton.addEventListener("click", () => {
+    navButton.classList.toggle("toggle");
+    console.log("running");
+  });
+} else {
+  const navButton = document.querySelector(".navbar-toggler");
+
+  navButton.addEventListener("click", () => {
+    navButton.classList.toggle("toggle");
+    console.log("running");
+  });
 }
-
-animateHeader();
-
-const columns = document.querySelectorAll(".columns");
-const controller = new ScrollMagic.Controller();
-const absorb = gsap.timeline({
-  defaults: {
-    duration: 0.5,
-  },
-});
-columns.forEach((column) => {
-  absorb.fromTo(
-    column,
-    { opacity: 0, y: "10%", scale: 0.3 },
-    { opacity: 1, y: "0", scale: 1 }
-  );
-
-  absorbScene = new ScrollMagic.Scene({
-    triggerElement: column,
-    triggerHook: 1,
-    reverse: false,
-  })
-
-    .setTween(absorb)
-    .addTo(controller);
-});
-
-const navButton = document.querySelector(".navbar-toggler");
-
-navButton.addEventListener("click", () => {
-  navButton.classList.toggle("toggle");
-});
