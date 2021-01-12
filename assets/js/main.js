0a1090c722decc48942949b86384d5983a63cced
shani__tripathi <shani.tripathi01@gmail.com>
2021-01-12 14:49:40 +0000

    design changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,45 @@
-if (
-  window.location.pathname === "/" ||
-  window.location.pathname === "/index.html"
-) {
-  const headerName = document.querySelector(".header-name");
+const headerName = document.querySelector(".header-name");
 
-  const text = "Hi, I am Shani!";
-  const speed = 50;
-  var i = 0;
+const text = "Hi, I am Shani!";
+const speed = 50;
+var i = 0;
 
-  function animateHeader() {
-    if (i < text.length) {
-      headerName.innerHTML += text[i];
-      i++;
+function animateHeader() {
+  if (i < text.length) {
+    headerName.innerHTML += text[i];
+    i++;
 
-      setTimeout(animateHeader, speed);
-    }
+    setTimeout(animateHeader, speed);
   }
-
-  animateHeader();
-
-  const columns = document.querySelectorAll(".columns");
-  const controller = new ScrollMagic.Controller();
-  const absorb = gsap.timeline({
-    defaults: {
-      duration: 0.5,
-    },
-  });
-  columns.forEach((column) => {
-    absorb.fromTo(column, { opacity: 0, y: "10%" }, { opacity: 1, y: "0" });
-
-    absorbScene = new ScrollMagic.Scene({
-      triggerElement: column,
-      triggerHook: 1,
-      reverse: false,
-    })
-
-      .setTween(absorb)
-      .addTo(controller);
-  });
-
-  const navButton = document.querySelector(".navbar-toggler");
-
-  navButton.addEventListener("click", () => {
-    navButton.classList.toggle("toggle");
-  });
-} else {
-  const navButton = document.querySelector(".navbar-toggler");
-
-  navButton.addEventListener("click", () => {
-    navButton.classList.toggle("toggle");
-  });
 }
+
+animateHeader();
+
+const columns = document.querySelectorAll(".columns");
+const controller = new ScrollMagic.Controller();
+const absorb = gsap.timeline({
+  defaults: {
+    duration: 0.5,
+  },
+});
+columns.forEach((column) => {
+  absorb.fromTo(column, { opacity: 0, y: "10%" }, { opacity: 1, y: "0" });
+
+  absorbScene = new ScrollMagic.Scene({
+    triggerElement: column,
+    triggerHook: 1,
+    reverse: false,
+  })
+
+    .setTween(absorb)
+    .addTo(controller);
+});
+
+const navButton = document.querySelector(".navbar-toggler");
+
+navButton.addEventListener("click", () => {
+  navButton.classList.toggle("toggle");
+});
 
 // function sendEmail(name, email, message) {
 //   Email.send({
